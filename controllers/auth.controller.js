0d618c58921e8c8a3184bbb77913fd02a92289e5
Kevin O <Kevinnuricumbo@gmail.com>
2022-11-14 21:42:53 -0600

    servioio de login creada

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,48 @@
 import {response} from 'express'
+import { bdCliente } from '../models/cliente.js';
+import { Token } from '../helpers/generar-JWT.js';
+import bcryptjs from 'bcryptjs';
 
 const login = async (req, res = response) => {
 
-    res.json({
-        msg: 'Login OK'
-    })
+    const {mail, password} = req.body;
+
+    try{
+
+        // Verificar si el email existe
+        const cliente = await bdCliente.cliente.findOne({where: {mail}});
+
+        if(!cliente){
+            return res.status(400).json({
+                msg: 'Correo o contraseña no son correctos - correo'
+            });
+        }
+
+        // Verificar contraseña
+        const validPassword = bcryptjs.compareSync(password, cliente.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Correo o contraseña no son correctos - password'
+            });
+        }
+
+        //const {password, ...restoCliente} = cliente
+
+        //Generar Token
+        const token = await Token.generarJWT(cliente.id);
+
+        res.json({
+            cliente,
+            token
+        })
+
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Algo salio mal'
+        });
+    }
 }
 
 export const authController = {
